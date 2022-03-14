af0ee68e961e617b0e5b59f3c44d1d44fdd00955
Matheus <matheus.granda@gmail.com>
2022-03-14 10:47:20 -0300

    tela-login

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,47 @@
+import React, {Children, useContext} from "react";
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom"; 
+import LoginPage from './pages/loginPages';
+import HomePage from './pages/loginPages/homePage';
+import { AuthProvider, AuthContext } from "./context/auth";
+
+const AppRoutes = () =>{
+
+const Private = ({children}) => {
+    const { authenticated, loading } = useContext(AuthContext);
+
+    if(loading) {
+    return <div className="loading">Carregando...</div>; 
+    }
+
+    if(!authenticated) {
+        return <Navigate to= "/login"/>;
+    }
+
+    return Children;
+
+
+};
+    
+    
+    return(
+        <Router>
+            <AuthProvider >
+            <Routes>
+                <Route exact path="/login" element={<LoginPage />} />
+                <Route exact path="/" element={<Private>
+                    <HomePage /> 
+                    </Private>} />
+
+            </Routes>
+            </AuthProvider>
+        </Router>
+    );
+}
+
+export default AppRoutes; 
